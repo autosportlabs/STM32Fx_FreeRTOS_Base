d3cc19524e5c1b690fe40ec33e061f6dae4c0c39
Jeff Ciesielski <jeffciesielski@gmail.com>
2013-12-23 13:47:02 -0800

    Add build configuration files for libraries

---
+++ libs.mk
@@ -0,0 +1,6 @@
+# This is a sort of meta package for all of the libraries we produce
+# in the 'libs' folder.  To add a new library, see existing library
+# config files for details on build requirements
+include libs/stm32f4_periph.mk
+include libs/freertos.mk
+include libs/stm32_usb.mk
