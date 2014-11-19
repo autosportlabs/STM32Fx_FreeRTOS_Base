9573b04d6bd42cac52e68ba6f8629dc16e4e8926
Jeff Ciesielski <jeffciesielski@gmail.com>
2014-11-18 19:17:19 -0800

    Make periph librarie makefiles cross platform

--- libs/stm32f0_periph.mk
+++ libs/stm32f0_periph.mk
@@ -4,15 +4,13 @@
 
 ifneq ($(STM32F0XX_LIBS),)
 
-F0_PERIPH_BASE ?= libs/STM32F0xx_StdPeriph_LibV1.4.0/Libraries/
+F0_PERIPH_BASE ?= libs/STM32F0xx_StdPeriph_LibV1.4.0/Libraries
 F0_CMSIS_BASE = $(F0_PERIPH_BASE)/CMSIS
 F0_CMSIS_DEVICE_BASE = $(F0_CMSIS_BASE)/Device/ST/STM32F0xx
 F0_LIB_BASE = $(F0_PERIPH_BASE)/STM32F0xx_StdPeriph_Driver
 
-F0_PERIPH_SOURCES := $(shell find $(F0_LIB_BASE)/src -name '*.c' -type f -printf "%f\n")
+F0_PERIPH_SOURCES := $(wildcard $(F0_LIB_BASE)/src/*.c)
 F0_CMSIS_SOURCES  = $(F0_CMSIS_DEVICE_BASE)/Source/Templates/system_stm32f0xx.c
-
-
 
 ifeq ($(CPU), stm32f030)
 F0_CMSIS_STARTUP_SOURCES = $(F0_CMSIS_DEVICE_BASE)/Source/Templates/gcc_ride7/startup_stm32f030.s
@@ -30,7 +28,6 @@ ifeq ($(CPU), stm32f072)
 F0_CMSIS_STARTUP_SOURCES = $(F0_CMSIS_DEVICE_BASE)/Source/Templates/gcc_ride7/startup_stm32f072.s
 endif
 
-
 # Std periph includes
 LIB_INCLUDES += -I$(F0_LIB_BASE)/inc
 
@@ -38,12 +35,10 @@ LIB_INCLUDES += -I$(F0_LIB_BASE)/inc
 LIB_INCLUDES += -I$(F0_CMSIS_BASE)/Include
 LIB_INCLUDES += -I$(F0_CMSIS_BASE)/Device/ST/STM32F0xx/Include
 
-STM32F0_PERIPH_SRCS = $(addprefix $(F0_LIB_BASE)/src/,$(F0_PERIPH_SOURCES))
-STM32F0_PERIPH_SRCS += $(F0_CMSIS_SOURCES)
+STM32F0_PERIPH_SRCS = $(F0_CMSIS_SOURCES) $(F0_PERIPH_SOURCES)
 
 STM32F0_PERIPH_OBJS = $(STM32F0_PERIPH_SRCS:.c=.o) $(F0_CMSIS_STARTUP_SOURCES:.s=.o)
 
 BASE_LIBS += stm32f0_periph
-
 
 endif
