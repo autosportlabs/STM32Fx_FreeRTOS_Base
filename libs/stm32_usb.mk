7b335a7741bc3ccb0f2976b2acb20e8cf1551c83
Vadim Lobanov <vlobanov@localhost.localdomain>
2015-01-27 23:55:29 -0800

    Replace all $(shell find ...) instances with $(wildcard ...) equivalents. We never need the recursive nature of find, and the majority of them were iffy in that they'd find all files that end with 'c', not just C source files.

--- libs/stm32_usb.mk
+++ libs/stm32_usb.mk
@@ -11,30 +11,30 @@ ifneq ($(STM32_USB_DEV),)
 ST_USB_DEV_BASE = $(ST_USB_BASE)/STM32_USB_Device_Library
 
 #Core libraries
-STM32_USB_C_FILES += $(shell find $(ST_USB_DEV_BASE)/Core/src -name '*c')
+STM32_USB_C_FILES += $(wildcard $(ST_USB_DEV_BASE)/Core/src/*.c)
 LIB_INCLUDES      += -I$(ST_USB_DEV_BASE)/Core/inc/
 
 #Class libraries
 ST_USB_DEV_CLASS_BASE = $(ST_USB_DEV_BASE)/Class
 
 #Audio
-STM32_USB_C_FILES += $(shell find $(ST_USB_DEV_CLASS_BASE)/audio/src -name '*c')
+STM32_USB_C_FILES += $(wildcard $(ST_USB_DEV_CLASS_BASE)/audio/src/*.c)
 LIB_INCLUDES      += -I$(ST_USB_DEV_CLASS_BASE)/audio/inc/
 
 #CDC
-STM32_USB_C_FILES += $(shell find $(ST_USB_DEV_CLASS_BASE)/cdc/src -name '*c')
+STM32_USB_C_FILES += $(wildcard $(ST_USB_DEV_CLASS_BASE)/cdc/src/*.c)
 LIB_INCLUDES      += -I$(ST_USB_DEV_CLASS_BASE)/cdc/inc/
 
 #DFU
-STM32_USB_C_FILES += $(shell find $(ST_USB_DEV_CLASS_BASE)/dfu/src -name '*c')
+STM32_USB_C_FILES += $(wildcard $(ST_USB_DEV_CLASS_BASE)/dfu/src/*.c)
 LIB_INCLUDES      += -I$(ST_USB_DEV_CLASS_BASE)/dfu/inc/
 
 #HID
-STM32_USB_C_FILES += $(shell find $(ST_USB_DEV_CLASS_BASE)/hid/src -name '*c')
+STM32_USB_C_FILES += $(wildcard $(ST_USB_DEV_CLASS_BASE)/hid/src/*.c)
 LIB_INCLUDES      += -I$(ST_USB_DEV_CLASS_BASE)/hid/inc/
 
 #MSC
-STM32_USB_C_FILES += $(shell find $(ST_USB_DEV_CLASS_BASE)/msc/src -name '*c')
+STM32_USB_C_FILES += $(wildcard $(ST_USB_DEV_CLASS_BASE)/msc/src/*.c)
 LIB_INCLUDES      += -I$(ST_USB_DEV_CLASS_BASE)/msc/inc/
 
 endif #USB_DEV
@@ -44,18 +44,18 @@ ifneq ($(STM32_USB_HOST),)
 ST_USB_HOST_BASE = $(ST_USB_BASE)/STM32_USB_HOST_Library
 
 #Core libraries
-STM32_USB_C_FILES += $(shell find $(ST_USB_HOST_BASE)/Core/src -name '*c')
+STM32_USB_C_FILES += $(wildcard $(ST_USB_HOST_BASE)/Core/src/*.c)
 LIB_INCLUDES      += -I$(ST_USB_HOST_BASE)/Core/inc/
 
 #Class Libraries
 ST_USB_HOST_CLASS_BASE = $(ST_USB_HOST_BASE)/Class
 
 #HID
-STM32_USB_C_FILES += $(shell find $(ST_USB_HOST_CLASS_BASE)/HID/src -name '*c')
+STM32_USB_C_FILES += $(wildcard $(ST_USB_HOST_CLASS_BASE)/HID/src/*.c)
 LIB_INCLUDES      += -I$(ST_USB_HOST_CLASS_BASE)/HID/inc/
 
 #MSC
-STM32_USB_C_FILES += $(shell find $(ST_USB_HOST_CLASS_BASE)/MSC/src -name '*c')
+STM32_USB_C_FILES += $(wildcard $(ST_USB_HOST_CLASS_BASE)/MSC/src/*.c)
 LIB_INCLUDES      += -I$(ST_USB_HOST_CLASS_BASE)/MSC/inc/
 
 endif #USB_HOST
@@ -64,7 +64,7 @@ ifneq ($(STM32_USB_OTG),)
 #OTG Library
 ST_USB_OTG_BASE = $(ST_USB_BASE)/STM32_USB_OTG_Driver
 
-STM32_USB_C_FILES += $(shell find $(ST_USB_OTG_BASE)/src -name '*.c')
+STM32_USB_C_FILES += $(wildcard $(ST_USB_OTG_BASE)/src/*.c)
 LIB_INCLUDES      += -I$(ST_USB_OTG_BASE)/inc
 
 endif #USB_OTG
