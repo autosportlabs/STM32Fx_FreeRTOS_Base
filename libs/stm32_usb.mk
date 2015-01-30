106cd5e5169928563db809832cd118f7cd3f5b34
Vadim Lobanov <vlobanov@localhost.localdomain>
2015-01-29 20:55:40 -0800

    Exclude *template.c files from the libstm32_usb deliverable. This still doesn't let it build due to tons of missing defines and headers, but hey, at least the build system is there for whoever wants to tackle this next.

--- libs/stm32_usb.mk
+++ libs/stm32_usb.mk
@@ -4,6 +4,8 @@
 
 ifneq ($(STM32_USB_LIBS),)
 
+valid_c_files = $(sort $(filter-out $(wildcard $(1)/*template.c), $(wildcard $(1)/*.c)))
+
 ST_USB_BASE = $(LIB_PATH)/STM32_USB-Host-Device_Lib_V2.1.0/Libraries
 
 ifneq ($(STM32_USB_DEV),)
@@ -11,30 +13,30 @@ ifneq ($(STM32_USB_DEV),)
 ST_USB_DEV_BASE = $(ST_USB_BASE)/STM32_USB_Device_Library
 
 #Core libraries
-STM32_USB_C_FILES += $(sort $(wildcard $(ST_USB_DEV_BASE)/Core/src/*.c))
+STM32_USB_C_FILES += $(call valid_c_files,$(ST_USB_DEV_BASE)/Core/src)
 LIB_INCLUDES      += -I$(ST_USB_DEV_BASE)/Core/inc/
 
 #Class libraries
 ST_USB_DEV_CLASS_BASE = $(ST_USB_DEV_BASE)/Class
 
 #Audio
-STM32_USB_C_FILES += $(sort $(wildcard $(ST_USB_DEV_CLASS_BASE)/audio/src/*.c))
+STM32_USB_C_FILES += $(call valid_c_files,$(ST_USB_DEV_CLASS_BASE)/audio/src)
 LIB_INCLUDES      += -I$(ST_USB_DEV_CLASS_BASE)/audio/inc/
 
 #CDC
-STM32_USB_C_FILES += $(sort $(wildcard $(ST_USB_DEV_CLASS_BASE)/cdc/src/*.c))
+STM32_USB_C_FILES += $(call valid_c_files,$(ST_USB_DEV_CLASS_BASE)/cdc/src)
 LIB_INCLUDES      += -I$(ST_USB_DEV_CLASS_BASE)/cdc/inc/
 
 #DFU
-STM32_USB_C_FILES += $(sort $(wildcard $(ST_USB_DEV_CLASS_BASE)/dfu/src/*.c))
+STM32_USB_C_FILES += $(call valid_c_files,$(ST_USB_DEV_CLASS_BASE)/dfu/src)
 LIB_INCLUDES      += -I$(ST_USB_DEV_CLASS_BASE)/dfu/inc/
 
 #HID
-STM32_USB_C_FILES += $(sort $(wildcard $(ST_USB_DEV_CLASS_BASE)/hid/src/*.c))
+STM32_USB_C_FILES += $(call valid_c_files,$(ST_USB_DEV_CLASS_BASE)/hid/src)
 LIB_INCLUDES      += -I$(ST_USB_DEV_CLASS_BASE)/hid/inc/
 
 #MSC
-STM32_USB_C_FILES += $(sort $(wildcard $(ST_USB_DEV_CLASS_BASE)/msc/src/*.c))
+STM32_USB_C_FILES += $(call valid_c_files,$(ST_USB_DEV_CLASS_BASE)/msc/src)
 LIB_INCLUDES      += -I$(ST_USB_DEV_CLASS_BASE)/msc/inc/
 
 endif #USB_DEV
@@ -44,18 +46,18 @@ ifneq ($(STM32_USB_HOST),)
 ST_USB_HOST_BASE = $(ST_USB_BASE)/STM32_USB_HOST_Library
 
 #Core libraries
-STM32_USB_C_FILES += $(sort $(wildcard $(ST_USB_HOST_BASE)/Core/src/*.c))
+STM32_USB_C_FILES += $(call valid_c_files,$(ST_USB_HOST_BASE)/Core/src)
 LIB_INCLUDES      += -I$(ST_USB_HOST_BASE)/Core/inc/
 
 #Class Libraries
 ST_USB_HOST_CLASS_BASE = $(ST_USB_HOST_BASE)/Class
 
 #HID
-STM32_USB_C_FILES += $(sort $(wildcard $(ST_USB_HOST_CLASS_BASE)/HID/src/*.c))
+STM32_USB_C_FILES += $(call valid_c_files,$(ST_USB_HOST_CLASS_BASE)/HID/src)
 LIB_INCLUDES      += -I$(ST_USB_HOST_CLASS_BASE)/HID/inc/
 
 #MSC
-STM32_USB_C_FILES += $(sort $(wildcard $(ST_USB_HOST_CLASS_BASE)/MSC/src/*.c))
+STM32_USB_C_FILES += $(call valid_c_files,$(ST_USB_HOST_CLASS_BASE)/MSC/src)
 LIB_INCLUDES      += -I$(ST_USB_HOST_CLASS_BASE)/MSC/inc/
 
 endif #USB_HOST
@@ -64,7 +66,7 @@ ifneq ($(STM32_USB_OTG),)
 #OTG Library
 ST_USB_OTG_BASE = $(ST_USB_BASE)/STM32_USB_OTG_Driver
 
-STM32_USB_C_FILES += $(sort $(wildcard $(ST_USB_OTG_BASE)/src/*.c))
+STM32_USB_C_FILES += $(call valid_c_files,$(ST_USB_OTG_BASE)/src)
 LIB_INCLUDES      += -I$(ST_USB_OTG_BASE)/inc
 
 endif #USB_OTG
